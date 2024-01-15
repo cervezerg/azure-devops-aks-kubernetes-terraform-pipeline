a1c3110675324084ea5b6724421a7c58bdba6863
Cervezerg <javier.martinez.c@outlook.com>
2024-01-15 01:11:29 -0300

    fix to cluster and ad files

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -11,7 +11,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     name       = "systempool"
     vm_size    = "Standard_DS2_v2"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
-    availability_zones   = [1, 2, 3]
+    zones   = [1, 2, 3]
     enable_auto_scaling  = true
     max_count            = 3
     min_count            = 1
@@ -34,23 +34,15 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 # Identity (System Assigned or Service Principal)
   identity { type = "SystemAssigned" }
 
-# Add On Profiles
-  addon_profile {
-    azure_policy { enabled = true }
-    oms_agent {
-      enabled                    = true
-      log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
-    }
-  }
+oms_agent {
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
+}
 
 # RBAC and Azure AD Integration Block
-role_based_access_control {
-  enabled = true
-  azure_active_directory {
-    managed                = true
-    admin_group_object_ids = [azuread_group.aks_administrators.id]
-  }
-}  
+azure_active_directory_role_based_access_control {
+  managed = true
+  admin_group_object_ids = [ azuread_group.aks_administrators.id ]
+}
 
 # Windows Admin Profile
 windows_profile {
